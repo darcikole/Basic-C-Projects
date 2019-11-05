323500afde63617fbf8d5b915c204c6e5de3c7f7
darcikole <53584516+darcikole@users.noreply.github.com>
2019-11-04 17:03:27 -0800

    Step 34
    C# step 34

---
+++ AccountingApp1/AccountingApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccountingApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems" + "\r\n" + "Remember, we're \"accounting\" on you!");
+        }
+    }
+}
