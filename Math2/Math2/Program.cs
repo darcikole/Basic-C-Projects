ebe96415263a674eb8db5816637d390e51c46f39
darcikole <53584516+darcikole@users.noreply.github.com>
2019-11-05 16:29:19 -0800

    Math 2 drill
    C# drill 67

---
+++ Math2/Math2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Math2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program" + "\r\n");
+            Thread.Sleep(1500);
+
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("What is your Hourly Rate?");
+            decimal rate1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("How many hours worked per week?");
+            decimal hours1 = Convert.ToDecimal(Console.ReadLine());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("What is your Hourly Rate?");
+            decimal rate2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("How many hours worked per week?");
+            decimal hours2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("\r\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Weekly salary of Person 1: ");
+            decimal salary1 = rate1 * hours1;
+            Console.WriteLine(salary1 + "\r\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Weekly salary of Person 2: ");
+            decimal salary2 = rate2 * hours2;
+            Console.WriteLine(salary2 + "\r\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?" + "\r\n");
+            bool salaryComp = salary1 > salary2;
+            Console.WriteLine(salaryComp);
+
+
+
+
+        }
+    }
+}
--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -28,29 +28,29 @@ namespace VariablesAndDataTypes
             //Console.WriteLine(questMark);
             //Console.ReadLine();
 
-		Console.WriteLine("The Tech Academy" + "\r\n" + "Student Daily Report");
+		    Console.WriteLine("The Tech Academy" + "\r\n" + "Student Daily Report");
 
-		Console.WriteLine("What course are you on?");
-		string yourCourse = Console.ReadLine();
+		    Console.WriteLine("What course are you on?");
+		    string yourCourse = Console.ReadLine();
 
-		Console.WriteLine("What page are you on?");
-		string yourPage = Console.ReadLine();
+		    Console.WriteLine("What page are you on?");
+            int yourPage = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
-            	string needHelp = Console.ReadLine();
+		    Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
           
 
-            	Console.WriteLine("Were there any positive experiences you'd like to share?  Please give specifics.");
-            	string yourExperience = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share?  Please give specifics.");
+            string yourExperience = Console.ReadLine();
 
-            	Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific.");
-            	string yourFeedback = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific.");
+            string yourFeedback = Console.ReadLine();
 
-            	Console.WriteLine("How may hours did you study today?");
-            	var yourHours = Console.ReadLine();
+            Console.WriteLine("How may hours did you study today?");
+            int yourHours = Convert.ToInt32(Console.ReadLine());
            
 
-            	Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly." + "\r\n" + "Have a great day!");
+            Console.WriteLine("Thank you for your answers.  An Instructor will respond to this shortly." + "\r\n" + "Have a great day!");
 
 
         }
