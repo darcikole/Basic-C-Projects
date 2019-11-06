00fefed744f9220ef94c6c6e1aec081ae45b4e6b
darcikole <53584516+darcikole@users.noreply.github.com>
2019-11-06 14:50:14 -0800

    C# drill 75
    Boolean Logic

---
+++ BooleanLogic75/BooleanLogic75/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace BooleanLogic75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int myAge = Convert.ToInt32(age);
+            Console.WriteLine("\r\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\".");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("\r\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            string tickets = Console.ReadLine();
+            int speedTix = Convert.ToInt32(tickets);
+            Console.WriteLine("\r\n");
+            Thread.Sleep(1000);
+
+            bool qualified = myAge > 15 && DUI == false && speedTix <= 3;
+
+            Console.WriteLine("Are you qualified?");
+            Thread.Sleep(1000);
+            Console.WriteLine(qualified);
+
+
+            
+            //int bankAccountBalance = 500;
+            //bool isInARockBand = false;
+            //string name == “Jesse”;
+
+            //bool isReallyCool = (bankAccountBalance > 100000 || isInARockBand || name == “Jesse”)
+
+            //true
+        }
+    }
+}
