814bbdbe8f4a50694837bc769e8c43e07ef10b7f
darcikole <53584516+darcikole@users.noreply.github.com>
2019-11-06 17:19:35 -0800

    Do While Loop 94
    C# Boolean Loops page 94

---
+++ BooleanLoops94/BooleanLoops94/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace BooleanLoops94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int x = 0;
+            while (x < 5)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            Console.WriteLine("\r\n");
+            Thread.Sleep(1500);
+
+
+            Console.WriteLine("Guess how many bottles of beer on the wall?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool beers = number == 0;
+
+            do
+            {
+                switch (number)
+                {
+                    case 0:
+                        Console.WriteLine("You guessed Correct! We drank them all!");
+                        beers = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope, take one down and pass it around.");
+                        Console.WriteLine("Guess again!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!beers);
+
+            Console.Read();
+        }
+    }
+}
--- Branching92/Branching92/Program.cs
+++ Branching92/Branching92/Program.cs
@@ -22,17 +22,14 @@ namespace Branching92
 
             Console.WriteLine("Please enter the package width:");
             double width = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\r\n");
             Thread.Sleep(1000);
 
             Console.WriteLine("Please enter the package height:");
             double height = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\r\n");
             Thread.Sleep(1000);
 
             Console.WriteLine("Please enter the package length:");
             double length = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\r\n");
             Thread.Sleep(1000);
 
             double sum = width + height + length;
