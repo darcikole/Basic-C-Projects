97508b1704d0700ad48938befcd8ac0743395c2b
darcikole <53584516+darcikole@users.noreply.github.com>
2019-11-15 12:03:11 -0800

    classes and methods 111
    c# drill page 111

---
+++ classesAndMethods111/classesAndMethods111/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace classesAndMethods111
+{
+    public class Circle
+    {
+
+        public double Diameter(double data)
+        {
+            data = data * 2;
+            return data;
+        }
+
+        public double Circumference(double data)
+        {
+            data = (2 * Math.PI) * data;
+            return data;
+        }
+
+        public double Area(double data)
+        {
+            data = Math.PI * (data * data);
+            return data;
+        }
+    }
+}
---
+++ classesAndMethods111/classesAndMethods111/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace classesAndMethods111
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a number between 1 and 10.");
+            double Radius = Convert.ToDouble(Console.ReadLine());
+
+            Circle circleDiameter = new Circle();
+            double diameter = circleDiameter.Diameter(Radius);
+            Console.WriteLine("The Diameter of your circle is: " + diameter + "\r\n");
+            Thread.Sleep(1000);
+
+            Circle circleCircumference = new Circle();
+            double circumference = Math.Round(circleCircumference.Circumference(Radius), 2);
+            Console.WriteLine("The Circumference of your circle is: " + circumference + "\r\n");
+            Thread.Sleep(1000);
+
+            Circle circleArea = new Circle();
+            double area = Math.Round(circleArea.Area(Radius), 2);
+            Console.WriteLine("The Area of your circle is: " + area);
+
+
+
+            
+        }
+    }
+}
