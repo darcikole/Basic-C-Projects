2620e308acf2e6a5d8e4b87a821b9f45adcdafe5
darcikole <53584516+darcikole@users.noreply.github.com>
2019-12-04 16:18:42 -0800

    EnumExercise134
    C# page 134

--- EnumExercise134/EnumExercise134/Program.cs
+++ EnumExercise134/EnumExercise134/Program.cs
@@ -4,23 +4,38 @@ namespace EnumExercise134
 {
     class Program
     {
+        public enum Day
+        {
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
+        }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Please enter the current day of the week.");
+            string userDay = Console.ReadLine();
+            string today = userDay.ToLower();
 
+            Day day;
+            try
+            {
+                day = (Day)Enum.Parse(typeof(Day), today);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            
+                
 
-
+            
         }
-        public enum Day
-        {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
+       
     }
 }
