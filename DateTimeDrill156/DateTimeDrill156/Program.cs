af470845e550097b80d1a236139c8280362beb6c
darcikole <53584516+darcikole@users.noreply.github.com>
2019-12-10 13:20:52 -0800

    DateTime drill #156
    C# drill page 156

---
+++ DateTimeDrill156/DateTimeDrill156/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTimeDrill156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("\r\n" + "Please enter a whole number.");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("In {0} hour(s) it will be: {1}", hours, DateTime.Now.AddHours(hours));
+
+        }
+    }
+}
