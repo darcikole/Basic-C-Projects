486b07d0f5829f26865885189fe78818d93c8b68
darcikole <53584516+darcikole@users.noreply.github.com>
2020-01-19 11:15:32 -0800

    Update BirthYear.cs

--- ExceptionHandling165/ExceptionHandling165/BirthYear.cs
+++ ExceptionHandling165/ExceptionHandling165/BirthYear.cs
@@ -6,24 +6,19 @@ namespace ExceptionHandling165
         public BirthYear()
         {
             Console.WriteLine("What is your age?");
-            int age = 0;
             bool validAge = false;
             while (!validAge)
-            { 
-                validAge = int.TryParse(Console.ReadLine(), out age);
+            {
+                validAge = int.TryParse(Console.ReadLine(), out int age);
                 DateTime todaysDate = DateTime.Today;
                 DateTime birthYear = todaysDate.AddYears(-age);
                 DateTime yourYear = birthYear;
-                
 
-                if (age <= 0)   
+                if (age <= 0)
                 {
                     throw new AgeException();
                 }
-                else
-                {
-                    Console.WriteLine("You were born in {0} ", yourYear.Year);
-                }
+                Console.WriteLine("You were born in {0} ", yourYear.Year);
             }
         }
     }
